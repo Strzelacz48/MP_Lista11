f99c5d2fb26880bfe13cb4c56a74ab53c01bca80
Strzelacz48 <marcin@DESKTOP-FT92BPV.localdomain>
2022-05-31 03:22:11 +0200

    Moze wystarczy

--- zad_5.rkt
+++ zad_5.rkt
@@ -6,19 +6,14 @@
 ;; abstract syntax -------------------------------
 
 (define-type Op
-  (add)
-  (sub)
-  (mul)
-  (div)
-  (eql)
-  (leq))
+  (add) (sub) (mul) (div) (eql) (leq))
 
 (define-type Exp
   (numE [n : Number])
   (opE [op : Op] [l : Exp] [r : Exp])
   (ifE [b : Exp] [l : Exp] [r : Exp])
   (varE [x : Symbol])
-  (letE [x : Symbol] [e1 : Exp] [e2 : Exp]))
+  (letE [e : Exp][args : (Listof (Symbol * Exp))] ))
 
 ;; parse ----------------------------------------
 
@@ -36,8 +31,8 @@
           (parse (fourth (s-exp->list s))))]
     [(s-exp-match? `SYMBOL s)
      (varE (s-exp->symbol s))]
-    [(s-exp-match? `{let SYMBOL ANY ANY} s)
-     (letE (s-exp->symbol (second (s-exp->list s)))
+    [(s-exp-match? `{let ANY ...} s)
+     (letE (s-exp->symbol (second (s-exp->list s)));tutaj trzeba jeszcze doknoczyc na pewno
            (parse (third (s-exp->list s)))
            (parse (fourth (s-exp->list s))))]
     [else (error 'parse "invalid input")]))
@@ -84,25 +79,6 @@
   (numV [n : Number])
   (boolV [b : Boolean]))
 
-(define-type Binding
-  (bind [name : Symbol]
-        [val : Value]))
-
-;; environments
-
-(define-type-alias Env (Listof Binding))
-
-(define mt-env empty)
-(define (extend-env [env : Env] [x : Symbol] [v : Value]) : Env
-  (cons (bind x v) env))
-(define (lookup-env [n : Symbol] [env : Env]) : Value
-  (type-case (Listof Binding) env
-    [empty (error 'lookup "unbound variable")]
-    [(cons b rst-env) (cond
-                        [(eq? n (bind-name b))
-                         (bind-val b)]
-                        [else (lookup-env n rst-env)])]))
-
 ;; primitive operations
 
 (define (op-num-num->proc [f : (Number Number -> Number)]) : (Value Value -> Value)
@@ -137,6 +113,25 @@
     [(div) (op-num-num->proc /)]
     [(eql) (op-num-bool->proc =)]
     [(leq) (op-num-bool->proc <=)]))
+
+;; environments
+
+(define-type Binding
+  (bind [name : Symbol]
+        [val : Value]))
+
+(define-type-alias Env (Listof Binding))
+
+(define mt-env empty)
+(define (extend-env [env : Env] [x : Symbol] [v : Value]) : Env
+  (cons (bind x v) env))
+(define (lookup-env [n : Symbol] [env : Env]) : Value
+  (type-case (Listof Binding) env
+    [empty (error 'lookup "unbound variable")]
+    [(cons b rst-env) (cond
+                        [(eq? n (bind-name b))
+                         (bind-val b)]
+                        [else (lookup-env n rst-env)])]))
 
 ;; evaluation function
 
@@ -193,123 +188,3 @@
 
 (define (main [e : S-Exp]) : Void
   (print-value (eval (parse e) mt-env)))
-
-;; lexical addressing —————————————————————————————————
-
-;; target
-
-(define-type ExpA
-  (numA [n : Number])
-  (opA [op : Op] [l : ExpA] [r : ExpA])
-  (ifA [b : ExpA] [l : ExpA] [r : ExpA])
-  (varA [n : Number])
-  (letA [a1 : ExpA] [a2 : ExpA]))
-
-;; environments (lists of entities)
-
-(define-type-alias (EnvA 'a) (Listof 'a))
-
-(define mt-envA empty)
-
-(define (extend-envA [env : (EnvA 'a)] [x : 'a]) : (EnvA 'a)
-  (cons x env))
-
-(define (lookup-envA [n : Number] [env : (EnvA 'a)]) : 'a
-  (list-ref env n))
-
-;; evaluation function
-
-(define (evalA [a : ExpA] [env : (EnvA Value)]) : Value
-  (type-case ExpA a
-    [(numA n) (numV n)]
-    [(opA o l r) ((op->proc o) (evalA l env) (evalA r env))]
-    [(ifA b l r)
-     (type-case Value (evalA b env)
-       [(boolV v)
-        (if v (evalA l env) (evalA r env))]
-       [else
-        (error 'eval "type error")])]
-    [(varA n)
-     (lookup-envA n env)]
-    [(letA e1 e2)
-     (let ([v1 (evalA e1 env)])
-       (evalA e2 (extend-envA env v1)))]))
-
-(define (runA [s : S-Exp]) : Value
-  (evalA (translate (parse s) mt-envA) mt-envA))
-
-;; translation function
-
-(define (address-of [x : Symbol] [env : (EnvA Symbol)]) : Number
-  (type-case (EnvA Symbol) env
-    [empty
-     (error 'address-of "unbound variable")]
-    [(cons y rst-env)
-     (if (eq? x y)
-         0
-         (+ 1 (address-of x rst-env)))]))
-
-(define (translate [e : Exp] [env : (EnvA Symbol)]) : ExpA
-  (type-case Exp e
-    [(numE n)
-     (numA n)]
-    [(opE o l r)
-     (opA o (translate l env) (translate r env))]
-    [(ifE b l r)
-     (ifA (translate b env)
-          (translate l env)
-          (translate r env))]
-    [(varE x)
-     (varA (address-of x env))]
-    [(letE x e1 e2)
-     (letA (translate e1 env)
-           (translate e2 (extend-envA env x)))]))
-
-;; detranslate
-
-(define (detranslate [e : ExpA] [v : Number]) : Exp
-  (type-case ExpA e
-    [(numA n)
-     (numE n)]
-    [(opA o l r)
-     (opE o (detranslate l v) (detranslate r v))]
-    [(ifA b l r)
-     (ifE (detranslate b v)
-          (detranslate l v)
-          (detranslate r v))]
-    [(varA x)
-     (varE (string->symbol (generator-names (- v (+ x 1)))))]
-    [(letA e1 e2)
-     (letE
-      (string->symbol (generator-names v))
-      (detranslate e1 v)
-      (detranslate e2 (+ v 1)))]))
-
-(define (generator-names [x : Number]) : String
-  (if (eq? x 0)
-      ""
-      (string-append "x" (generator-names (- x 1)))));; koniec nowych rzeczy
-
-(module+ test
-  (test (run `2)
-        (runA `2))
-  (test (run `{+ 2 1})
-        (runA `{+ 2 1}))
-  (test (run `{* 2 1})
-        (runA `{* 2 1}))
-  (test (run `{+ {* 2 3} {+ 5 8}})
-        (runA `{+ {* 2 3} {+ 5 8}}))
-  (test (run `{= 0 1})
-        (runA `{= 0 1}))
-  (test (run `{if {= 0 1} {* 3 4} 8})
-        (runA `{if {= 0 1} {* 3 4} 8}))
-  (test (run `{let x 1 {+ x 1}})
-        (runA `{let x 1 {+ x 1}}))
-  (test (run `{let x 1 {+ x {let y 2 {* x y}}}})
-        (runA `{let x 1 {+ x {let y 2 {* x y}}}}))
-  (test (run `{let x 1
-                {+ x {let x {+ x 1}
-                       {* x 3}}}})
-        (runA `{let x 1
-                 {+ x {let x {+ x 1}
-                        {* x 3}}}})))
